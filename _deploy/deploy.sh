2fb9633d3931f8ea8f6f68bc6dcf7a5867c6427c
Ade Bateman <bateman@acm.org>
2017-09-30 13:47:55 -0700

    Configure lftp to upload built files

--- _deploy/deploy.sh
+++ _deploy/deploy.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
-set LFTP_HOME=`realpath ./_deploy`
-lftp -e "dir adrianba.net/wwwroot;exit" ftp.adrianba.net -u $LFTP_USER --env-password
+#!/usr/bin/env bash
+set -e # halt script on error
+
+BASEDIR=$(dirname "$0")
+mkdir ~/.lftp
+cp $BASEDIR/rc ~/.lftp
+cp $BASEDIR/adrianba.net.crt ~/.lftp
+
+lftp -e "mirror -R -p -P=20 --verbose=1 ./_site/. beta.adrianba.net/wwwroot/;exit" ftp.adrianba.net -u $LFTP_USER,$LFTP_PASSWORD
