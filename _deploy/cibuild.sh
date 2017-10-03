6a36af3299f45784406148b35c7bdfa61d48e2e3
Ade Bateman <bateman@acm.org>
2017-10-03 14:43:01 -0700

    Add htmlproofer to CI build.

--- _deploy/cibuild.sh
+++ _deploy/cibuild.sh
@@ -2,4 +2,6 @@
 set -e # halt script on error
 
 bundle exec jekyll build
+bundle exec htmlproofer ./_site --disable-external --allow-hash-href --empty-alt-ignore --internal-domains adrianba.net
+
 ls -al ./_site
