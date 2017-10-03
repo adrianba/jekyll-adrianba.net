3db06722cca86396180af104239655b13cd3eeef
Ade Bateman <bateman@acm.org>
2017-10-03 15:53:40 -0700

    Fix if then

--- _deploy/cibuild.sh
+++ _deploy/cibuild.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-if [ -f .cache/.jekyll-metadata ] then
-  cp .cache/.jekyll-metadata .
-fi
+# Copy metadata if cached
+[ -f .cache/.jekyll-metadata ] && cp .cache/.jekyll-metadata .
 
 bundle exec jekyll build --incremental
 bundle exec htmlproofer ./_site --disable-external --allow-hash-href --empty-alt-ignore --internal-domains adrianba.net
