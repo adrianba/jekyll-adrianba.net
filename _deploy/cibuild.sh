c67a6a3d8e7378d890021899f236f08d9a45eb88
Ade Bateman <bateman@acm.org>
2017-10-03 15:50:07 -0700

    Update cache info.

--- _deploy/cibuild.sh
+++ _deploy/cibuild.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
+if [ -f .cache/.jekyll-metadata ] then
+  cp .cache/.jekyll-metadata .
+fi
+
 bundle exec jekyll build --incremental
 bundle exec htmlproofer ./_site --disable-external --allow-hash-href --empty-alt-ignore --internal-domains adrianba.net
 
