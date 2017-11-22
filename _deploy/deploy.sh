13507b82a257ab75ce3dee14e1b8a58097959d97
Adrian Bateman <adrianba@microsoft.com>
2017-11-22 13:14:02 -0800

    Updated deployment to use git publish branch.

--- _deploy/deploy.sh
+++ _deploy/deploy.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-BASEDIR=$(dirname "$0")
-mkdir ~/.lftp
-cp $BASEDIR/rc ~/.lftp
-cp $BASEDIR/adrianba.net.crt ~/.lftp
-
-lftp -e "mirror -R -p -n -e -P=20 --verbose=1 --ignore-time ./_site/. adrianba.net/wwwroot/;exit" ftp.adrianba.net -u $LFTP_USER,$LFTP_PASSWORD
+# Update publish branch
+STATUS=`git log -1 --pretty=oneline`
+cd _site
+git config user.name "Travis-CI"
+git config user.email "travis-ci"
+git add .
+git commit -m "Built by Travis-CI: $STATUS"
+git status
+GH_REPO="@github.com/adrianba/jekyll-adrianba.net.git"
+FULL_REPO="https://$GH_TOKEN$GH_REPO"
+git push $FULL_REPO publish:publish
