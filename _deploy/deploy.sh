72cc6be6f4dbdb49f87f0fab7a7353ef5f117db3
Ade Bateman <bateman@acm.org>
2020-08-03 14:17:35 -0700

    Update CI to GitHub Action

--- _deploy/deploy.sh
+++ _deploy/deploy.sh
@@ -4,10 +4,10 @@ set -e # halt script on error
 # Update publish branch
 STATUS=`git log -1 --pretty=oneline`
 cd _site
-git config user.name "Travis-CI"
-git config user.email "travis-ci"
+git config user.name "GitHub Action"
+git config user.email "GitHub Action"
 git add .
-git commit -m "Built by Travis-CI: $STATUS"
+git commit -m "Built by GitHub Action: $STATUS"
 git status
 GH_REPO="@github.com/adrianba/jekyll-adrianba.net.git"
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
